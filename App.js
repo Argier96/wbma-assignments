8728bf308e8efa6c7a88880d35e118da039a3ab7
Ritesh Ghimire <argier96@Riteshs-Laptop.local>
2023-01-09 11:07:10 +0200

    Added component list and moved object from flatlist into list.js and is called on app.js

--- App.js
+++ App.js
@@ -1,83 +1,12 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
+import List from './Component/list';
+
 const App = () => {
   return (
     <>
       <View style={styles.container}>
-        <FlatList
-          data={mediaArray}
-          renderItem={({item}) => {
-            return (
-              <TouchableOpacity style={{backgroundColor:'grey', marginTop:10, height:400}}>
-                <Image
-                  style={{width: 220, height: 380, margin: 10}}
-                  source={{uri: item.thumbnails.w160}}
-                />
-                <View>
-                  <Text
-                    style={{
-                      fontStyle: 'bold',
-                      fontSize: 25,
-                      right: -250,
-                      top: -370,
-                    }}
-                  >
-                    {item.title}
-                  </Text>
-                  <Text
-                    style={{
-                      right: -240,
-                      top: -370,
-                      marginRight: 250,
-                      fontSize: 15,
-                    }}
-                  >
-                    {item.description}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            );
-          }}
-        />
+        <List />
       </View>
       <StatusBar style="auto" />
     </>
