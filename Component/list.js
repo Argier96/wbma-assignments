0ab03d04aacd0c4d32de9ce1c0177b3f6217e166
Ritesh Ghimire <argier96@Riteshs-Laptop.local>
2023-01-09 11:48:28 +0200

    first app completed

--- Component/list.js
+++ Component/list.js
@@ -1,5 +1,6 @@
-import {Text, View, Image, TouchableOpacity, FlatList} from 'react-native';
+import {FlatList} from 'react-native';
 import React from 'react';
+import ListItem from './listItem';
 
 const mediaArray = [
   {
@@ -38,40 +39,7 @@ const List = () => {
     <>
       <FlatList
         data={mediaArray}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity
-              style={{backgroundColor: 'grey', marginTop: 10, height: 400}}
-            >
-              <Image
-                style={{width: 220, height: 380, margin: 10}}
-                source={{uri: item.thumbnails.w160}}
-              />
-              <View>
-                <Text
-                  style={{
-                    fontStyle: 'bold',
-                    fontSize: 25,
-                    right: -250,
-                    top: -370,
-                  }}
-                >
-                  {item.title}
-                </Text>
-                <Text
-                  style={{
-                    right: -240,
-                    top: -370,
-                    marginRight: 250,
-                    fontSize: 15,
-                  }}
-                >
-                  {item.description}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
+        renderItem={({item}) => <ListItem singleMedia={item} />}
       />
     </>
   );
---
+++ Component/listItem.js
@@ -0,0 +1,44 @@
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ListItem = (props) => {
+  const item = props.singleMedia;
+  return (
+    <TouchableOpacity
+      style={{backgroundColor: 'grey', marginTop: 10, height: 400}}
+    >
+      <Image
+        style={{width: 220, height: 380, margin: 10}}
+        source={{uri: item.thumbnails.w160}}
+      />
+      <View>
+        <Text
+          style={{
+            fontStyle: 'bold',
+            fontSize: 25,
+            right: -250,
+            top: -370,
+          }}
+        >
+          {item.title}
+        </Text>
+        <Text
+          style={{
+            right: -240,
+            top: -370,
+            marginRight: 250,
+            fontSize: 15,
+          }}
+        >
+          {item.description}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+export default ListItem;
